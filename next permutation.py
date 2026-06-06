3b2b9662c9451fc428b9310806600b33119857d1
Bhakti Shrivastava <shrivastavabhakti764@gmail.com>
2026-06-06 21:57:17 +0530

    https://leetcode.com/problems/next-permutation/description/

---
+++ next permutation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+
+        # Step 1: Find the first decreasing element from the right
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # Step 2: If found, find the next greater element from the right
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Step 3: Reverse the suffix
+        left = i + 1
+        right = n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
