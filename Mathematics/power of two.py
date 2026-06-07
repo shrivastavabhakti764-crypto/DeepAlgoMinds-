d5517a8f81c1f97d3534d1e8f8b9d2f82791dd80
Bhakti Shrivastava <shrivastavabhakti764@gmail.com>
2026-06-07 18:00:42 +0530

    https://leetcode.com/problems/power-of-two/

---
+++ Mathematics/power of two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        
+        for i in range (1,n):
+
+            if 2**i == n:
+                return True
+        return False    
